fc400a81bcb34ad2d5ab6b33bf5ac22312cc3fd1
Dorukan Kılınç <144803266+dorukanklnc@users.noreply.github.com>
2026-01-09 20:53:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,13 @@
 
 static void printZones(const std::vector<ZoneCount>& v) {
     std::cout << "TOP_ZONES\n";
-
     for (auto& x : v)
         std::cout << x.zone << "," << x.count << "\n";
 }
 
-static void printSlots(const std::vector<SlotCount>& v) 
+static void printSlots(const std::vector<SlotCount>& v)
 {
     std::cout << "TOP_SLOTS\n";
-
     for (auto& x : v)
         std::cout << x.zone << "," << x.hour << "," << x.count << "\n";
 }
@@ -22,7 +20,6 @@ int main() {
     auto t0 = std::chrono::high_resolution_clock::now();
 
     TripAnalyzer analyzer;
-
     analyzer.ingestFile("SmallTrips.csv");
 
     printZones(analyzer.topZones(10));
